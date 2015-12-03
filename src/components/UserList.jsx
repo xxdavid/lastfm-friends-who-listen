56ed099a631435e0fe594d05b93caf962b26d478
David Pavlík <david@dpavlik.cz>
2015-12-03 16:19:35 +0100

    Sort users by play count

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -4,7 +4,11 @@ import User from './User.jsx';
 
 export default class UserList extends React.Component {
     render() {
-        var userNodes = this.props.data.map(function (user) {
+        var data = this.props.data;
+        data.sort(function (a, b) {
+            return (a.count < b.count) ? 1 : -1;
+        });
+        var userNodes = data.map(function (user) {
             return (
                 <User key={user.key} name={user.name} count={user.count} />
             );
