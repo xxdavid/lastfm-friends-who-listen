b1d9bc5784d7713c0f6f8d9c6a4889d2fbd61ded
David Pavlík <david@dpavlik.cz>
2016-01-13 19:19:08 +0100

    Revert disabling automatic component state comparison

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,10 +3,6 @@ var React = require('react');
 import * as language from './../language';
 
 export default class User extends React.Component {
-    shouldComponentUpdate() {
-        return false;
-    }
-
     render() {
         var styles = {
             table: {
