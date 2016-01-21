33e858fc9357d86cbdcaa3db9aa05a5f3fb4ac72
David Pavlík <david@dpavlik.cz>
2016-01-21 18:42:12 +0100

    Don't use MutationObserver if the target is undefined

--- src/observeDomChanges.js
+++ src/observeDomChanges.js
@@ -1,14 +1,16 @@
 export default function observeDomChanges(callback) {
     var target = document.querySelector('div#content.main-content');
 
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.addedNodes.length > 0) {
-                callback();
-            }
+    if (target) {
+        var observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                if (mutation.addedNodes.length > 0) {
+                    callback();
+                }
+            });
         });
-    });
 
-    var config = { childList: true };
-    observer.observe(target, config);
+        var config = {childList: true};
+        observer.observe(target, config);
+    }
 }
