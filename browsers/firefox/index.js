89198d59079ab4672ad0c1ece502bed0727c60a3
David Pavlík <david@dpavlik.cz>
2017-02-16 16:16:19 +0100

    Support for HTTPS version of Last.fm, fixes #3

--- browsers/firefox/index.js
+++ browsers/firefox/index.js
@@ -1,7 +1,7 @@
 var pageMod = require('sdk/page-mod');
 
 pageMod.PageMod({
-    include: 'http://www.last.fm/*',
+    include: ['http://www.last.fm/*', 'https://www.last.fm/*'],
     contentScriptFile: './content.js',
     contentScriptWhen: 'ready'
 });
